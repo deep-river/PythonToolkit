838ef2a5363f40768417495459b7237de822d9e7
OguriR <by.brad.li@gmail.com>
2022-06-01 12:08:29 +0800

    Update minimize.py

--- minimize.py
+++ minimize.py
@@ -1,6 +1,19 @@
 import sys
-from PIL import Image as im
-from PIL import ImageSequence
+try:
+    from PIL import Image as im
+    from PIL import ImageSequence
+except ModuleNotFoundError:
+    print("Could not find library: [%s]. Trying to install it... \nTry disabling system proxy if any error occurs. \n" % "pillow")
+
+    try:
+        from pip import main as pipmain
+    except:
+        from pip._internal.main import main as pipmain
+    # main.main(['install', '-r', 'requirements.txt'])
+    pipmain(['install', 'pillow'])
+
+    from PIL import Image as im
+    from PIL import ImageSequence
 
 
 def minimize(filename, threshold = [10, 24, 48]):
@@ -34,7 +47,7 @@ def minimize(filename, threshold = [10, 24, 48]):
         else:
             resized = img.resize((w, h), 1)
             resized.save(new_filename, format=img_format)
-        print("Done: %s", new_filename)
+        print("[Done]: %s" % new_filename)
 
 
 def gif_resize(w, h, frames):
@@ -46,5 +59,5 @@ def gif_resize(w, h, frames):
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         minimize(sys.argv[1])
-    if len(sys.argv) == 3:
+    elif len(sys.argv) == 3:
         minimize(sys.argv[1], sys.argv[2])
